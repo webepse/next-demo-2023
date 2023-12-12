9bf1704f9117171130e06c11c6db5bd4d9805e0a
berti <berti@CLASSES.LOCAL>
2023-12-12 09:36:37 +0100

    exercice link reviews

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,9 +1,15 @@
+import Link from 'next/link'
+
 export default function ReviewsPage() 
 {
     return (
         <>
             <h1>Reviews</h1>
             <p>Here we'll list all the reviews</p>
+            <ul>
+                <li><Link href="/reviews/diablo">Diablo 4</Link></li>
+                <li><Link href="/reviews/spider-man">SpiderMan</Link></li>
+            </ul>
         </>
     )
 }

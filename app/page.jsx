f8c433e3b078f366a3130da5759b668c6bdbc9ae
berti <berti@CLASSES.LOCAL>
2023-12-12 09:31:14 +0100

    link

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,4 @@
-"use client"
-import { useEffect } from "react"
-
 export default function HomePage() {
-    useEffect(() => {
-        window.alert("Bienvenue sur mon site")
-    })
-
     console.log("HomePage [Reading]")
 
     return (

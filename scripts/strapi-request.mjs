eb5c641d9878a9b801cff1a184cc2484b434cab3
berti <berti@CLASSES.LOCAL>
2023-12-20 09:37:40 +0100

    getReview(slug)

--- scripts/strapi-request.mjs
+++ scripts/strapi-request.mjs
@@ -3,10 +3,11 @@ import qs from 'qs'
 
 // const url = "http://localhost:1337/api/reviews" + '?populate=*'
 const url = "http://localhost:1337/api/reviews" + '?' + qs.stringify({
-    fields: ['slug', 'title', 'subtitle', 'publishedAt'],
+    filters: {slug: { $eq: 'diablo-4'}},
+    fields: ['slug', 'title', 'subtitle', 'publishedAt','body'],
     populate: { image: {fields: ['url']}},
     sort: ['publishedAt:desc'],
-    pagination: { pageSize: 6}
+    pagination: { pageSize: 1, withCount: false}
 },{encodeValuesOnly : true})
 const response = await fetch(url)
 const body = await response.json()
